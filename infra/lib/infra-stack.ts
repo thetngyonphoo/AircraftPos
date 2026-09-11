be1a5a5c662fc8f90c0d0bf70bd19adde0939243
Thet Ngyon Phoo <66239761+thetngyonphoo@users.noreply.github.com>
2026-09-11 18:04:11 +0630

    commit Ignore build artifacts

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,7 +22,54 @@ export class InfraStack extends cdk.Stack {
       code: lambda.Code.fromAsset(
         '../src/AircraftPos.ParserLambda/bin/Release/net8.0/publish'
       ),
+      timeout: cdk.Duration.seconds(30),
     });
+
+    // Parsed DynamoDB table
+    const parsedTable = new dynamodb.Table(this, 'ParsedTable', {
+      partitionKey: {
+        name: 'flightId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'timestamp',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    // Allow Parser Lambda to write to DynamoDB
+    parsedTable.grantWriteData(parserLambda);
+
+    // Give Lambda the table name
+    parserLambda.addEnvironment(
+      'PARSED_TABLE_NAME',
+      parsedTable.tableName
+    );
+
+    // Dead-Letter Queue
+    const deadLetterQueue = new sqs.Queue(this, 'PosDeadLetterQueue', {
+      retentionPeriod: cdk.Duration.days(14),
+    });
+
+    // Main SQS Queue
+    const posQueue = new sqs.Queue(this, 'PosQueue', {
+      visibilityTimeout: cdk.Duration.seconds(30),
+
+      deadLetterQueue: {
+        queue: deadLetterQueue,
+        maxReceiveCount: 3,
+      },
+    });
+
+    // Allow Parser Lambda to send messages to SQS
+    posQueue.grantSendMessages(parserLambda);
+
+    // Give Lambda the SQS Queue URL
+    parserLambda.addEnvironment(
+      'SQS_QUEUE_URL',
+      posQueue.queueUrl
+    );
 
     // Allow Lambda to read/write the bucket
     posbucket.grantReadWrite(parserLambda);
--- src/AircraftPos.ParserLambda/Function.cs
+++ src/AircraftPos.ParserLambda/Function.cs
@@ -1,9 +1,13 @@
 using AircraftPos.Application.Interfaces;
 using AircraftPos.Application.Services;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -14,10 +18,14 @@ namespace AircraftPos.ParserLambda
     public class Function
     {
         private readonly IAmazonS3 _s3Client;
+        private readonly IAmazonDynamoDB _dynamoDBClient;
+        private readonly IAmazonSQS _sqsClient;
         private readonly IPosReportParser _posReportParser;
         public Function()
         {
             _s3Client = new AmazonS3Client();
+            _dynamoDBClient = new AmazonDynamoDBClient();
+            _sqsClient = new AmazonSQSClient();
             _posReportParser = new PosReportParser();
         }
 
@@ -29,7 +37,7 @@ namespace AircraftPos.ParserLambda
         /// <returns></returns>
         public async Task FunctionHandler(S3Event s3Event, ILambdaContext context)
         {
-            foreach(var record in s3Event.Records)
+            foreach (var record in s3Event.Records)
             {
                 var bucketName = record.S3.Bucket.Name;
                 var objectKey = record.S3.Object.Key;
@@ -67,6 +75,68 @@ namespace AircraftPos.ParserLambda
                         ContentType = "application/json"
                     });
                 context.Logger.LogInformation($"Attachment saved to S3: {bucketName}/{attachmentKey}");
+
+                //Save parsed record to DynamoDB
+                var tableName = Environment.GetEnvironmentVariable("PARSED_TABLE_NAME");
+
+                if (string.IsNullOrWhiteSpace(tableName))
+                {
+                    throw new InvalidOperationException(
+                        "PARSED_TABLE_NAME environment variable is not configured.");
+                }
+
+                var timestamp = posReport.Timestamp.ToString("yyyy-MM-ddTHH:mm:ssZ");
+
+                var putItemRequest = new PutItemRequest
+                {
+                    TableName = tableName,
+                    Item = new Dictionary<string, AttributeValue>
+                    {
+                        ["flightId"] = new AttributeValue
+                        {
+                            S = posReport.FlightId
+                        },
+                        ["timestamp"] = new AttributeValue
+                        {
+                            S = timestamp
+                        },
+                        ["attachment"] = new AttributeValue
+                        {
+                            S = attachmentKey
+                        }
+                    }
+                };
+
+                await _dynamoDBClient.PutItemAsync(putItemRequest);
+
+                context.Logger.LogInformation($"Parsed record saved to DynamoDB: {posReport.FlightId}");
+
+                // Send message to SQS
+                var queueUrl = Environment.GetEnvironmentVariable("SQS_QUEUE_URL");
+
+                if (string.IsNullOrWhiteSpace(queueUrl))
+                {
+                    throw new InvalidOperationException(
+                        "SQS_QUEUE_URL environment variable is not configured.");
+                }
+
+                var message = new
+                {
+                    flightId = posReport.FlightId,
+                    timestamp = timestamp
+                };
+
+                var messageBody = JsonSerializer.Serialize(message);
+
+                await _sqsClient.SendMessageAsync(
+                    new SendMessageRequest
+                    {
+                        QueueUrl = queueUrl,
+                        MessageBody = messageBody
+                    });
+
+                context.Logger.LogInformation($"Message sent to SQS: {messageBody}");
+
             }
         }
     }
